a3abbd61d69e2bd3a93fa9ad269fcc31c9234c31
Carlos Coves Prieto <escorponox@gmail.com>
2015-01-12 22:22:03 +0100

    Added control Tags

---
+++ src/main/java/actions/Bean.java
@@ -0,0 +1,25 @@
+package actions;
+
+/**
+ * Created by Carlos Coves Prieto on 1/12/15.
+ */
+public class Bean {
+    private int id;
+    private String property;
+
+    public String getProperty() {
+        return property;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
